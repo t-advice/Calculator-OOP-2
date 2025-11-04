ebfc77d51d9673ca05ef4cf84af8ae103535bd90
Tashwill Advice <tashwilladvice02@gmail.com>
2025-11-04 14:58:52 +0200

    other methods added . Exception added at devision since a number cant be devided by zero

--- BasicCalculatorOOP/Calculator.cs
+++ BasicCalculatorOOP/Calculator.cs
@@ -20,5 +20,26 @@ namespace BasicCalculatorOOP
             return a + b;
         }
 
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        // remember exception handling for division by zero
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+
     }
 }
