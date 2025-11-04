186463ce455a8ba8336d23beca4398dbeeeefd81
Tashwill Advice <tashwilladvice02@gmail.com>
2025-11-04 15:05:16 +0200

    program class ( logic and operations) class coded

--- BasicCalculatorOOP/Program.cs
+++ BasicCalculatorOOP/Program.cs
@@ -6,6 +6,41 @@
         {
             // create new class instance(object)
             //of Calculator class
+            Calculator calc = new Calculator();
+
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number;");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose an operation(+,-,*,/): ");
+            string op = Console.ReadLine();
+
+            double result = 0;
+
+                switch (op)
+                {
+                     case "+":
+                    result = calc.Add(num1, num2);
+                    break;
+                    case "-":
+                    result = calc.Substract(num1, num2);
+                    break;
+                     case "*":
+                    result = calc.Multiply(num1, num2);
+                    break;
+                     case "/":
+                    result = calc.Divide(num1, num2);
+                    break;
+                     default:
+                    Console.WriteLine("Invalid operation.");
+                    return;
+                }
+                    Console.WriteLine($"Result: {result}");
+
+
+
 
 
         }
